6e1e2119d346d2090aff75f03f9f814bc20255d0
Mariana Valencia <marianavalenciah2010@gmail.com>
2026-05-28 16:53:12 -0500

    get_user_information : Add inputs and style

---
+++ Restaurante/js/inventario.js
@@ -0,0 +1,4 @@
+let codigo_producto = document.getElementById("codigo_producto").value;
+let nombre_producto = document.getElementById("nombre_producto").value;
+let cantidad = document.getElementById("cantidad").value;
+let marca_producto = document.getElementById("marca_producto").value;
---
+++ Restaurante/js/pedidos.js
@@ -0,0 +1,6 @@
+let producto = document.getElementById("producto").value;
+let nombre_usuario = document.getElementById("nombre_usuario").value;
+let direccion = document.getElementById("direccio").value;
+let cantidad = document.getElementById("cantidad").value;
+let metodo_pago = document.getElementsByName("metodo_pago").value;
+let telefono = document.getElementById("telefono").value;
