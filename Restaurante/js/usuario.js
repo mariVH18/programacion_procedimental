95951892d9c3c44745d5b1a9f9fc6ebf25d1e5ea
Mariana Valencia <marianavalenciah2010@gmail.com>
2026-06-04 15:40:10 -0500

    validate_module_user : Validate data

--- Restaurante/js/usuario.js
+++ Restaurante/js/usuario.js
@@ -8,3 +8,29 @@ let genero = document.getElementsByName("genero").value;
 let cargo = document.getElementsByName("cargo").value;
 let fecha_nacimiento = document.getElementsById("fecha_nacimiento").value;
 let contraseña = document.getElementsById("contraseña").value;
+
+function ValidarDatos(){
+    if(nombre == '' || apellido == '' || numero_documento == '' || telefono == '' || correo == ''){
+         console.log("Los campos están vacios")
+    } 
+    else {
+    if(nombre != /[a-zA-Z]/){
+        console.log("Los datos son incorrectos")
+    }
+    if(telefono.length() >= 10){
+        console.log("Los datos son incorrectos")
+    }
+    if(correo != /[@]/){
+        console.log("El correo es invalido")
+    }
+    if(apellido != /[a-zA-Z]/){
+        console.log("Los datos son incorrectos")
+    }
+    if(numero_documento != /^[0-9]+$/){
+        console.log("El numero de documento no puede tener letras")
+    }
+}
+}
+       
+
+
