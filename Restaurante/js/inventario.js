aa6a352d910d7a0f5da7b74188a7aa2a648986ac
Mariana Valencia <marianavalenciah2010@gmail.com>
2026-06-11 17:14:48 -0500

    validate_module_inventario : validate inputs

--- Restaurante/js/inventario.js
+++ Restaurante/js/inventario.js
@@ -2,3 +2,23 @@ let codigo_producto = document.getElementById("codigo_producto").value;
 let nombre_producto = document.getElementById("nombre_producto").value;
 let cantidad = document.getElementById("cantidad").value;
 let marca_producto = document.getElementById("marca_producto").value;
+
+
+function ValidarDatos(){
+
+    if(!/^[a-zA-Z]+$/.test(nombre_producto)){
+        console.log("El nombre del producto no debe tener numeros");
+    }
+
+    if(!/^[a-zA-Z]+$/.test(marca_producto)){
+        console.log("La marca del producto no debe tener numeros");
+    }
+
+    if(!/^[0-9]+$/.test(codigo_producto)){
+        console.log("El codigo del producto no puede tener letras");
+    }
+
+    if(!/^[0-9]+$/.test(cantidad)){
+        console.log("La cantidad no puede tener letras");
+    }
+}
